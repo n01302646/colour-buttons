1d582477945df503faf54d3397e0dead5f2f1155
Jobanjit Singh <n01302646@nj-00118-008.humber.org>
2020-03-10 15:31:17 -0400

    Completed

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
 
 //REFERENCES
-let redButton = document.getElementById('redc');
-let blueButton = document.getElementById('bluec');
-let greenButton = document.getElementById('greenc');
+let redButton = document.querySelector('#redc');
+let blueButton = document.querySelector('#bluec');
+let greenButton = document.querySelector('#greenc');
 
+function button_click(color) {
+    console.log('test')
+    document.querySelector('body').style.backgroundColor = color;
+}
 
+// redButton.onclick = button_click;
+// blueButton.onclick = button_click;
+// greenButton.onclick = button_click;
 
-redButton.onclick = redColor();
+redButton.addEventListener('click', function(event) { button_click('red') })
+blueButton.addEventListener('click', function(event) { button_click('green') })
+greenButton.addEventListener('click', function(event) { button_click('blue') })
 
-blueButton.onclick = document.body.style.backgroundColor = 'blue';
-
-greenButton.onclick = document.body.style.backgroundColor = 'green';
 
