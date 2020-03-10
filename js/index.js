56bd7376e7e2dc368ffc201f9f70108c4a42ab34
Jobanjit Singh <n01302646@nj-00118-008.humber.org>
2020-03-10 15:12:41 -0400

    added JS

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+//REFERENCES
+let redButton = document.getElementById('redc');
+let blueButton = document.getElementById('bluec');
+let greenButton = document.getElementById('greenc');
+
+
+
+redButton.onclick = redColor();
+
+blueButton.onclick = document.body.style.backgroundColor = 'blue';
+
+greenButton.onclick = document.body.style.backgroundColor = 'green';
+
